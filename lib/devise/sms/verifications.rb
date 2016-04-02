d479afda6b97b7f9f5a2b8c57e5ea8a7495c0bee
ravisingh47 <ravi.singh@bizongo.in>
2016-04-02 16:34:40 +0530

    Initial commit

---
+++ lib/devise/sms/verifications.rb
@@ -0,0 +1,9 @@
+require "devise/sms/verifications/version"
+
+module Devise
+  module Sms
+    module Verifications
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/devise/sms/verifications/version.rb
@@ -0,0 +1,7 @@
+module Devise
+  module Sms
+    module Verifications
+      VERSION = "0.1.0"
+    end
+  end
+end
